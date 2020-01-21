427bcef1c1cc64e6c88a760ff8a01df716e9bbc7
Vinicius Monteiro Arjonas <vmonteiro@engebras.com.br>
2020-01-21 10:37:51 -0300

    feat: connection configuration with mongo

--- src/app.js
+++ src/app.js
@@ -1,15 +1,27 @@
 import express from 'express';
 import routes from './routes';
+import mongoose from 'mongoose';
+import databaseConfig from './config/database';
 
 class App {
   constructor() {
     this.server = express();
     this.middlewares();
+    this.databases();
     this.routes();
   }
 
   middlewares() {
     this.server.use(express.json());
+  }
+
+  databases() {
+    mongoose.connect(databaseConfig.database, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
   }
 
   routes() {
--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,3 @@
 module.exports = {
-  dialect: 'mysql',
-  host: 'localhost',
-  username: 'root',
-  password: '260692',
-  database: 'portalfinanceiro',
-  define: {
-    timestamps: true,
-    underscored: true,
-    underscoredAll: true,
-  },
+  database: 'mongodb://localhost:27017/sky',
 };
